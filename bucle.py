1c4697550ffa3ec6e4ae58aba950bba3d7da3db8
labfut.dpn@gmail.com <labfut.dpn@gmail.com>
2020-08-13 18:08:23 -0500

    primer bucle

---
+++ bucle.py
@@ -0,0 +1,9 @@
+
+valor = int(input())
+while valor % 3 == 0:
+    print (f"El {valor} es multiplo de 3")
+    a = valor + 3
+    print (f"Y el siguiente numero multiplo de 3 es {a}")
+    break
+   
+  
